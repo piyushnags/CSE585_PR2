98fcb296d21649fa230e8abe7e2da16d6b6072cb
Piyush <piyushnags@gmail.com>
2023-02-15 14:09:09 -0500

    Adding images and thin.m

---
+++ thin.m
@@ -0,0 +1,23 @@
+%--------------------------------------------------------------------------
+% Module: thin.m
+% Usage: thin(f, A, B)
+% Purpose: Homotopy preserving skeletonization
+%
+% Input Variables:
+%   f       input image array
+%   A       Structuring element for hit
+%   B       Structuring element for miss
+%
+% Returned Results:
+%   out     output image array
+%
+% Processing Flow:
+%   
+%
+% Author: Piyush Nagasubramaniam, Siyuan Hong, Jacky Lin
+% Date: 02/15/2023
+%--------------------------------------------------------------------------
+function [out] = thin(f, A, B)
+
+f_hm = hm_transform(f, A, B);
+out = f - f_hm;
